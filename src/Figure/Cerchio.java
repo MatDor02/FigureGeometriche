beb96e1675b57763a60e28dbf2cdd3c12351d0f9
Matteo D'Orazio <matteo.dorazio02@gmail.com>
2021-11-13 16:13:39 +0100

    created Cerchio class

--- src/Figure/Cerchio.java
+++ src/Figure/Cerchio.java
@@ -1,4 +1,25 @@
 package Figure;
 
-public class Cerchio {
+import java.lang.*;
+
+public class Cerchio extends Figura {
+
+	public double raggio;
+
+	public Cerchio(double raggio) {
+		super("Cerchio");
+		this.raggio = raggio;
+	}
+
+	public double getRaggio() {
+		return raggio;
+	}
+
+	public double getPerimetro() {
+		return 2 * raggio * Math.PI;
+	}
+
+	public double getArea() {
+		return Math.PI * Math.pow(raggio, 2);
+	}
 }
