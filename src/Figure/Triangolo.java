15ebc5b08f74880c6f12de8c97c3c9776e09a047
Matteo D'Orazio <matteo.dorazio02@gmail.com>
2021-11-14 16:17:12 +0100

    changed Triangolo's constructor

--- src/Figure/Triangolo.java
+++ src/Figure/Triangolo.java
@@ -1,26 +1,22 @@
 package Figure;
 
-import myExceptions.*;
+import java.lang.*;
 
 public class Triangolo extends Figura {
 
 
-	private double base, altezza, lato2, lato3;
+	private double lato1, lato2, lato3, altezza1;
 
-	public Triangolo(String nome, double altezza, double latoRelativoAdAltezza, double lato2, double lato3) {
+	public Triangolo(String nome, double lato1, double lato2, double lato3) {
 		super(nome);
-		base = latoRelativoAdAltezza;
-		this.altezza = altezza;
+		this.lato1 = lato1;
 		this.lato2 = lato2;
 		this.lato3 = lato3;
+		setAltezza1(lato1, lato2, lato3);
 	}
 
-	public double getBase() {
-		return base;
-	}
-
-	public double getAltezza() {
-		return altezza;
+	public double getLato1() {
+		return lato1;
 	}
 
 	public double getLato2() {
@@ -31,12 +27,23 @@ public class Triangolo extends Figura {
 		return lato3;
 	}
 
+	public double getAltezza1() {
+		return altezza1;
+	}
+
+	private void setAltezza1(double base, double latoSx, double latoDx) {
+		double  altezza, cosAlfa;
+		cosAlfa = (base*base + latoSx*latoSx - latoDx*latoDx)/(2 * base * latoSx);
+		altezza = latoSx * Math.sqrt(1 - cosAlfa * cosAlfa);
+		altezza1 = altezza;
+	}
+
 	public double getPerimetro() {
-		return base + lato2 + lato3;
+		return lato1 + lato2 + lato3;
 	}
 
 	public double getArea() {
-		return base * altezza / 2;
+		return lato1 * altezza1 / 2;
 	}
 }
 
--- src/Figure/TriangoloRettangolo.java
+++ src/Figure/TriangoloRettangolo.java
@@ -1,9 +1,14 @@
 package Figure;
 
+import java.lang.*;
+
 public class TriangoloRettangolo extends Triangolo {
 
-	public TriangoloRettangolo(double cateto1, double cateto2, double ipotenusa) {
-		super("Triangolo rettangolo", cateto2, cateto1, cateto2, ipotenusa);
+	public TriangoloRettangolo(double cateto1, double cateto2) {
+		super("Triangolo rettangolo", cateto1, cateto2,/*ipotenusa*/ Math.sqrt(cateto1*cateto1 + cateto2*cateto2));
 	}
 
+	public TriangoloRettangolo(char c, double cateto1, double ipotenusa) {
+		super("Triangolo rettangolo", cateto1, /* cateto2 */ Math.sqrt(ipotenusa*ipotenusa - cateto1*cateto1), ipotenusa);
+	}
 }
