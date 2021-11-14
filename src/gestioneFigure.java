1622c1ebf3947404afeca51d2a59880ad18a4b19
Matteo D'Orazio <matteo.dorazio02@gmail.com>
2021-11-14 16:55:00 +0100

    Definitive gestioneFigure

--- src/gestioneFigure.java
+++ src/gestioneFigure.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.lang.*;
 import Figure.*;
 import myExceptions.negativeSideException;
 import myExceptions.nullSideException;
@@ -7,13 +8,13 @@ import javax.swing.*;
 
 public class gestioneFigure {
 
-    private static Figura[] figure = new Figura[3];
+    private static Figura[] figure = new Figura[10];
 
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
         int scelta = 99;
-        double misura1, misura2, misura3, misura4;
+        double misura1, misura2, misura3;
         Figura figura = null;
 
         System.out.println("\nCiao!");
@@ -36,6 +37,7 @@ public class gestioneFigure {
                 input.nextLine();
                 continue;
             }
+            input.nextLine(); // prendo l'invio dopo al nextInt andato a buon fine
 
             if (scelta < 0 || scelta > 6)
                 System.out.println("\nInserisci un numero compreso tra 0 e 6");
@@ -67,11 +69,38 @@ public class gestioneFigure {
                         break;
                     }
                     case 4: {
-                        misura1 = getPositiveDouble("Altezza");
-                        misura2 = getPositiveDouble(("Lato relativo al'altezza"));
-                        misura3 = getPositiveDouble("Secondo lato");
-                        misura4 = getPositiveDouble("Terzo lato");
-                        figura = new Triangolo("Triangolo", misura1, misura2, misura3, misura4);
+                        misura1 = getPositiveDouble(("Primo lato"));
+                        misura2 = getPositiveDouble("Secondo lato");
+                        boolean invalidMeasure;
+                        do {
+                            misura3 = getPositiveDouble("Terzo lato");
+                            invalidMeasure = !(isTriangle(misura1, misura2, misura3));
+                            if(invalidMeasure)
+                                System.out.println("\nTerzo lato non compatibile con i primi due, inserire una lunghezza congrua.");
+                        } while(invalidMeasure);
+
+                        figura = new Triangolo("Triangolo", misura1, misura2, misura3);
+                        break;
+                    }
+                    case 5: {
+                        misura1 = getPositiveDouble("Cateto");
+                        String element;
+                        do {
+                            System.out.print("\nVuoi inserire l'altro cateto o l'ipotenusa? [c/i]: ");
+                            element = input.nextLine();
+                            if(element.charAt(0) == 'c') {
+                                misura2 = getPositiveDouble("Altro cateto");
+                                figura = new TriangoloRettangolo(misura1, misura2);
+                            }
+                            else if(element.charAt(0) == 'i') {
+                                misura2 = getPositiveDouble(("Ipotenusa"));
+                                figura = new TriangoloRettangolo('i', misura1, misura2);
+                            }
+                            else
+                                System.out.println("Input non valido. Inserisci 'i' per l'ipotenusa, 'c' per l'altro cateto.");
+
+                        } while (element.charAt(0) != 'c' && element.charAt(0) != 'i');
+                        break;
                     }
 
                 }
@@ -130,6 +159,11 @@ public class gestioneFigure {
         } while(inCatch);
 
         return misura;
+    }
+
+    // Returns true if the parameters can be the sides of a triangle
+    private static boolean isTriangle(double lato1, double lato2, double lato3) {
+        return (lato3 < lato1 + lato2) && (lato3 > Math.abs(lato1 - lato2));
     }
 
 }
